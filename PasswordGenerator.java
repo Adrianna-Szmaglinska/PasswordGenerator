fcb8cc0bc3fd90a5f62a9e41f98d5dca93f52de9
Adrianna-Szmaglinska <154603275+Adrianna-Szmaglinska@users.noreply.github.com>
2024-05-14 19:42:28 +0000

    Typo fix.

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,6 +1,5 @@
 import java.security.SecureRandom;
 
-
 public class PasswordGenerator {
     private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
