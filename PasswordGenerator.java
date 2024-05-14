1f401bac139c97e0405a3716286afa4935f1802a
Adrianna-Szmaglinska <154603275+Adrianna-Szmaglinska@users.noreply.github.com>
2024-05-14 20:16:37 +0100

    Main file upload.

---
+++ PasswordGenerator.java
@@ -0,0 +1,48 @@
+import java.security.SecureRandom;
+
+
+public class PasswordGenerator {
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL = "!@#$%^&*()-_=+";
+
+    public static void main(String[] args) {
+        int length = 12; 
+        String generatedPassword = generatePassword(length);
+        System.out.println("Generated password: " + generatedPassword);
+    }
+
+    public static String generatePassword(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
+
+        // Add at least one character from each category
+        password.append(UPPER.charAt(random.nextInt(UPPER.length())));
+        password.append(LOWER.charAt(random.nextInt(LOWER.length())));
+        password.append(DIGITS.charAt(random.nextInt(DIGITS.length())));
+        password.append(SPECIAL.charAt(random.nextInt(SPECIAL.length())));
+
+        // Add remaining characters randomly
+        for (int i = 4; i < length; i++) {
+            String charSet = UPPER + LOWER + DIGITS + SPECIAL;
+            password.append(charSet.charAt(random.nextInt(charSet.length())));
+        }
+
+        // Shuffle the characters to make the password more secure
+        String shuffledPassword = shuffle(password.toString());
+
+        return shuffledPassword;
+    }
+
+    private static String shuffle(String input) {
+        char[] characters = input.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            int randomIndex = (int) (Math.random() * characters.length);
+            char temp = characters[i];
+            characters[i] = characters[randomIndex];
+            characters[randomIndex] = temp;
+        }
+        return new String(characters);
+    }
+}
